e79f6bd7ca98551af42775acecc5445cb4a5949b
Prajwal <prajwalkrishna2@gmail.com>
2020-08-04 13:19:34 +0530

    length of last word

---
+++ src/Easy_problems/LengthOfLastWord/Prajwal.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        int len=s.length(), lastLength=0;
+
+        while(len > 0 && s.charAt(len-1)==' '){
+            len--;
+        }
+
+        while(len > 0 && s.charAt(len-1)!=' '){
+            lastLength++;
+            len--;
+        }
+
+        return lastLength;
+    }
+}
