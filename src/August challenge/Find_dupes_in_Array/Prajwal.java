a934f9ded89e298915ef2d08b86b503e41fa14e1
Prajwal <prajwalkrishna2@gmail.com>
2020-08-06 15:20:13 +0530

    added comment in the code

--- src/August challenge/Find_dupes_in_Array/Prajwal.java
+++ src/August challenge/Find_dupes_in_Array/Prajwal.java
@@ -7,6 +7,8 @@ class Solution
         {
             /* flip the ith number to negative and if i-1th number is already negative
             then that number is a duplicate
+            This works because of the condition on the data in array
+            i.e., Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array)
              */
 
             int index = Math.abs(nums[i]) - 1;
